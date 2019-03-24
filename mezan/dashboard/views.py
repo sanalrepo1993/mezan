27c98ffae5485776198bec53d4fbc99b6a8e513f
venusdharan <venusdharan@gmail.com>
2019-03-25 04:49:12 +0530

    home page

--- mezan/dashboard/views.py
+++ mezan/dashboard/views.py
@@ -3,17 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 from masjid.models import Masjid
 from member.models import Family,Member
+from mahal.models import Mahal
 
 def dashboard(request):
     masjid_count = Masjid.objects.all().count()
     family_count = Family.objects.all().count()
     member_count = Member.objects.all().count()
+    mahal = Mahal.objects.get(id=1)
     context = {
         "title":"Dashboard",
         "navbar":True,
         "masjid_count":masjid_count,
         "family_count":family_count,
         "member_count":member_count,
+        'mahal':mahal,
     }
     return render(request,"dashboard/dashboard.html",context)
 
